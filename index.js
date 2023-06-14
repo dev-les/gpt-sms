f050296346b86d372539c2ec5b7840cca9fc72bf
dev-les <lesdev974@gmail.com>
2023-06-14 13:50:59 -0400

    integrating openAI API

--- index.js
+++ index.js
@@ -1,29 +1,37 @@
 const express = require('express')
 const dotenv = require('dotenv');
 const bodyParser = require('body-parser');
+const cors = require('cors');
+const { askGPT } = require('./openAI');
 const { MessagingResponse } = require('twilio').twiml;
 dotenv.config()
 
+const corsOption = {
+  origin: '*',
+};
 const app = express();
-
+app.use(cors(corsOption));
 app.use(bodyParser.urlencoded({ extended: false }));
 
 const port = process.env.port || 8080
 
-app.post('/sms', (req, res) => {
-    const twiml = new MessagingResponse();
-  
-    if (req.body.Body == 'hello') {
-      twiml.message('Hi!');
-    } else if (req.body.Body == 'bye') {
-      twiml.message('Goodbye');
-    } else {
-      twiml.message(
-        'No Body param match, Twilio sends this in the request to your server.'
-      );
-    }
-  
-    res.type('text/xml').send(twiml.toString());
+app.post('/sms', async (req, res) => {
+  try{
+      const twiml = new MessagingResponse();
+     if (req.body.Body) {
+        const query = req.body.Body;
+        const gptResponse = await askGPT(query);
+        twiml.message(gptResponse.content);
+      } else {
+        twiml.message(
+          'No Body param match, Twilio sends this in the request to your server.'
+        );
+      }
+      res.type('text/xml').send(twiml.toString());
+      // res.send(gptResponse.content)
+    } catch(error){
+    res.send('ERROR: '+ error);
+  }
   });
   
   app.listen(port, () => {
---
+++ openAI.js
@@ -0,0 +1,27 @@
+const { Configuration, OpenAIApi } =  require("openai");
+const dotenv = require("dotenv");
+dotenv.config();
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+  });
+const openai = new OpenAIApi(configuration);
+// const completion = await openai.createChatCompletion({
+//     model: "gpt-3.5-turbo",
+//     messages: [{role: "user", content: "what is the largest building in the world"}],
+//   });
+//   console.log(completion.data.choices[0].message);
+
+const askGPT = async (content) => {
+        try {
+            console.log(content);
+            const complete = await openai.createChatCompletion({
+                 model: "gpt-3.5-turbo",
+                 messages: [{role: "user", content: content}],
+            });
+            return complete.data.choices[0].message;
+            
+        } catch (error) {
+            console.log(error);
+        }     
+}
+module.exports = {askGPT}
